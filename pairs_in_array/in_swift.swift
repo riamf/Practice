f38968202cc61e4bafe4a4dec3ef58537a068072
Pawel Kowalczuk <pwl.kowalczuk@gmail.com>
2019-06-06 19:11:28 +0200

    find pairs 3 ways of solving

--- pairs_in_array/in_swift.swift
+++ pairs_in_array/in_swift.swift
@@ -16,9 +16,58 @@ func findPairs(sum: Int, in array: [Int]) -> [(Int, Int)] {
     return result
 }
 
-let array = Set((0..<20).map({ _ in
-                                Int(arc4random_uniform(10) + arc4random_uniform(10))
+func findPairsHash(sum: Int, in array: [Int]) -> [(Int, Int)] {
+
+    var result = [(Int, Int)]()
+
+    var hash = [Int: Int]()
+    for v in array {
+        let rem = sum - v
+        if let vc = hash[rem] {
+            result.append((rem, vc))
+        }
+        hash[v] = rem
+    }
+
+    return result
+}
+
+
+func findPairsSorting(sum: Int, in array: [Int]) -> [(Int, Int)] {
+
+    var result = [(Int, Int)]()
+    let sorted = array.sorted()
+
+    var i = 0
+    var j = sorted.count - 1
+
+    while i < j {
+        let current = sorted[i] + sorted[j]
+
+        if current == sum {
+            result.append((sorted[i], sorted[j]))
+        }
+        if current > sum {
+            j -= 1
+        } else {
+            i += 1
+        }
+    }
+
+
+    return result
+}
+
+let array =
+    Set((0..<20).map({ _ in
+                                Int(Int(arc4random_uniform(20)) * (arc4random_uniform(3) % 2 == 0 ? -1 : 1) + Int(arc4random_uniform(10)))
                             })).map({ $0 })
-let result = findPairs(sum: 11, in: array)
+var result = findPairs(sum: 11, in: array)
+print("N = \(array.count)")
 print(array)
 print(result)
+result = findPairsHash(sum: 11, in: array)
+print(result)
+result = findPairsSorting(sum: 11, in: array)
+print(result)
+
