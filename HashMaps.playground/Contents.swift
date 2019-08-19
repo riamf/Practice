ac17775218a78e6e9f5f1c71dcf2a28af3860cfc
Pawel Kowalczuk <pwl.kowalczuk@gmail.com>
2019-08-19 18:12:02 +0200

    task 2 from hash map challenge

--- HashMaps.playground/Contents.swift
+++ HashMaps.playground/Contents.swift
@@ -28,3 +28,19 @@ let note = ["give", "one", "grand", "today"]
 checkMagazine(magazine: magazine, note: note)
 
 
+func twoStrings(s1: String, s2: String) -> String {
+
+    let s1Set = Set(s1.map({$0}))
+    let s2Set = Set(s2.map({$0}))
+
+    if s1Set.intersection(s2Set).isEmpty {
+        return "NO"
+    } else {
+        return "YES"
+    }
+}
+
+twoStrings(s1: "hello", s2: "world")
+twoStrings(s1: "hi", s2: "world")
+
+
