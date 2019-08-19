d66da8835b963a9fedf1c8e1138979a58317dd67
Pawel Kowalczuk <pwl.kowalczuk@gmail.com>
2019-08-19 19:10:59 +0200

    anagrams working not optimal :(

--- HashMaps.playground/Contents.swift
+++ HashMaps.playground/Contents.swift
@@ -44,3 +44,53 @@ twoStrings(s1: "hello", s2: "world")
 twoStrings(s1: "hi", s2: "world")
 
 
+extension String {
+    func substring(start: Int, end: Int) -> String {
+        if start > end {
+            fatalError()
+        }
+        let sidx = index(startIndex, offsetBy: start)
+        let eidx = index(startIndex, offsetBy: end)
+        return String(self[sidx...eidx])
+    }
+}
+
+func sherlockAndAnagrams(s: String) -> Int {
+    var allSubstrings = [String]()
+    for i in (0..<s.count) {
+        allSubstrings.append(s.substring(start: i, end: i))
+    }
+
+    var cnt = 0
+    for i in (0..<allSubstrings.count) {
+        let s1 = allSubstrings[i]
+        for j in (i+1..<allSubstrings.count) {
+            let s2 = allSubstrings[j]
+            if isAnagram(s1: s1, s2: s2) {
+                print("\(s1) - \(s2)")
+                cnt += 1
+            }
+        }
+
+    }
+
+    return cnt
+}
+
+func isAnagram(s1: String, s2: String) -> Bool {
+    var s1Hash = [Character: Int]()
+    var s2Hash = [Character: Int]()
+    for ch in s1 {
+        s1Hash[ch, default: 0] += 1
+    }
+    for ch in s2 {
+        s2Hash[ch, default: 0] += 1
+    }
+
+    return s1Hash == s2Hash
+}
+
+print(sherlockAndAnagrams(s: "abba"))
+print(sherlockAndAnagrams(s: "ifailuhkqq"))
+print(sherlockAndAnagrams(s: "kkkk"))
+//print(sherlockAndAnagrams(s: "ifailuhkqqhucpoltgtyovarjsnrbfpvmupwjjjfiwwhrlkpekxxnebfrwibylcvkfealgonjkzwlyfhhkefuvgndgdnbelgruel"))
