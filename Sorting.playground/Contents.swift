dcbb77f63dcbab83d666502f208e1d5678da4ea2
Pawel Kowalczuk <pkowalczuk@applause.com>
2019-08-21 12:40:29 +0200

    count sorting and median search in unsorted array

--- Sorting.playground/Contents.swift
+++ Sorting.playground/Contents.swift
@@ -47,3 +47,86 @@ func maximumToys(prices: [Int], k: Int) -> Int {
 }
 
 print(maximumToys(prices: [1,12,5,111,200,1000,10], k: 50))
+
+print("activityNotifications")
+
+func activityNotifications(expenditure: [Int], d: Int) -> Int {
+    guard expenditure.count > d  else { return 0 }
+
+    var cnt = 0
+    var medianPosition = 0
+    let isEven = d % 2 == 0 ? true : false
+    if isEven {
+        medianPosition = d / 2
+    } else {
+        medianPosition = d / 2 + 1
+    }
+    var countArray = Array<Int>(repeating: 0, count: 201)
+
+    for i in (0..<d) {
+        countArray[expenditure[i]] += 1
+    }
+
+    var current = 0
+    var end = d
+
+    while end < expenditure.count {
+
+        var counter = 0
+        var left = 0
+        var right = 0
+        var median: Float = 0.0
+
+        while counter < medianPosition {
+            counter += countArray[left]
+            left += 1
+        }
+
+        right = left
+        left -= 1
+
+        if counter > medianPosition || !isEven {
+            median = Float(left)
+        } else {
+            while countArray[right] == 0 {
+                right += 1
+            }
+            median = Float(right + left) / 2.0
+        }
+
+        if Float(expenditure[end]) >= Float(2.0 * median) {
+            cnt += 1
+        }
+        countArray[expenditure[current]] -= 1
+        countArray[expenditure[end]] += 1
+        end += 1
+        current += 1
+    }
+
+    return cnt
+}
+
+func countSort(array: [Int], r: Int) -> [Int] {
+    var count = Array<Int>(repeating: 0, count: r)
+    for val in array {
+        count[val] += 1
+    }
+
+    for i in (1..<r) {
+        count[i] = count[i] + count[i-1]
+    }
+
+    var output = Array<Int>(repeating: 0, count: array.count)
+    for i in (0..<array.count).reversed() {
+        output[count[array[i]]-1] = array[i]
+        count[array[i]] = count[array[i]] - 1
+    }
+
+    return output
+
+}
+
+//print(activityNotifications(expenditure: [1,2,3,4,5], d: 3))
+//print(activityNotifications(expenditure: [10,20,30,40,50], d: 3))
+//print(activityNotifications(expenditure: [2,3,4,2,3,6,8,4,5], d: 5))
+print(activityNotifications(expenditure: [1,2,3,4,4], d: 4))
