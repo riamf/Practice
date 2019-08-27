0698dc7e4a254f41ffe9f9753361b3e932045948
Pawel Kowalczuk <pwl.kowalczuk@gmail.com>
2019-08-27 19:31:37 +0200

    dynamic programming  candies

--- DynamicProgramming.playground/Contents.swift
+++ DynamicProgramming.playground/Contents.swift
@@ -18,6 +18,85 @@ func maxSubsetSum(arr: [Int]) -> Int {
 //print(maxSubsetSum(arr: [1,2,3,4,5]))
 print(maxSubsetSum(arr: [-2,1,3,-4,5]))
 print(maxSubsetSum(arr: [3,5,-7,8,10]))
-print(maxSubsetSum(arr: [1,1,1,1,1,1]))
+//print(maxSubsetSum(arr: [1,1,1,1,1,1]))
 
+print("abbreviation")
+func abbreviation(a: String, b: String) -> String {
 
+    var mapACount = [Character: Int]()
+    for ch in a {
+        mapACount[ch, default: 0] += 1
+    }
+    var mapBCount = [Character: Int]()
+    for ch in b {
+        mapBCount[ch, default: 0] += 1
+    }
+
+    for ch in a {
+        if ch.isUppercase {
+            let aCount = mapACount[ch]!
+            if mapBCount[ch] == nil && mapBCount[ch.lowercased().first!] == nil {
+                return "NO"
+            }
+            if let bUpCount = mapBCount[ch], bUpCount < aCount {
+                return "NO"
+            }
+            if let bLowCount = mapBCount[ch.lowercased().first!], bLowCount < aCount {
+                return "NO"
+            }
+        }
+    }
+
+    for ch in b {
+        if ch.isUppercase {
+            let aCount = mapBCount[ch]!
+            if mapACount[ch] == nil && mapACount[ch.lowercased().first!] == nil {
+                return "NO"
+            }
+            if let bUpCount = mapACount[ch], bUpCount < aCount {
+                return "NO"
+            }
+            if let bLowCount = mapACount[ch.lowercased().first!], bLowCount < aCount {
+                return "NO"
+            }
+        }
+    }
+
+    return "YES"
+}
+
+print(abbreviation(a: "AbcDE", b: "ABDE"))
+print(abbreviation(a: "AbcDE", b: "AFDE"))
+
+print("candies")
+
+func candies(n: Int, arr: [Int]) -> Int {
+
+    var candies = 0
+
+    var st = 0
+    var nd = st + 1
+    while nd < arr.count {
+        let s = arr[st]
+        let e = arr[nd]
+        if s != e {
+            let dif = ceil(s > e ? Double(s) / Double(e) : Double(e) / Double(s))
+            candies += 1
+            candies += 2//Int(dif)
+        } else {
+            candies += 1
+            candies += 2
+        }
+        st += 2
+        nd += 2
+    }
+    if arr.count % 2 != 0 {
+        candies += 1
+    }
+
+    return candies
+}
+
+//print(candies(n: 5, arr: [1,2,2]))
+//print(candies(n: 5, arr: [4,6,4,5,6,2]))
+print(candies(n: 5, arr: [2,4,3,5,2,6,4,5]))
