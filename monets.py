1f88d0929e6d61bc4c053e495d866dd53cd44a86
Pawel Kowalczuk <pkowalczuk@applause.com>
2019-09-05 14:45:00 +0200

    monets solved again

--- monets.py
+++ monets.py
@@ -1,32 +1,48 @@
-from enum import Enum
-
-class Monets(Enum):
-    TEN = 10
-    FIVE = 5
-    TWO = 2
-
+from sys import maxsize
 
 def countMinimum(amount: int) -> int:
 
     if amount == 0:
         return 0
-    if amount == 1:
+    if amount == 1 or amount == 3:
         raise Exception("OMG")
     
+    return minCoins(coins=[10,5,2], V=amount)
+
+def minCoins(coins, V): 
+    m = len(coins)
+    if (V == 0): 
+        return 0
+  
+    res = maxsize 
+      
+    for c in coins: 
+        if (c <= V): 
+            sub_res = minCoins(coins, V-c)
+
+            if (sub_res != maxsize and sub_res + 1 < res): 
+                res = sub_res + 1
+  
+    return res 
     
-    if amount >= 10 and amount - 10 > 3:
-        rest = amount - 10
-        return 1 + countMinimum(rest)
-    if amount >= 5 and (amount - 5 > 3 or amount - 5 == 0):
-        rest = amount - 5
-        return 1 + countMinimum(rest)
-    if amount >= 2:
-        rest = amount - 2
-        return 1 + countMinimum(rest)
-    return 0
 
 
     
+print(countMinimum(amount=11))
 
-print("sol:")
-print(countMinimum(10))
+
+def memoizeMinCoins(coins: list, V: int) -> int:
+    memoize = [maxsize] * (V + 1)
+    memoize[0] = 0
+    
+    m = len(coins)
+    for i in range(1, V+1):
+        for c in coins:
+            if c <= i:
+                sub_res = memoize[i - c]
+                if sub_res != maxsize and sub_res + 1 < memoize[i]:
+                    memoize[i] = sub_res + 1
+    return memoize[V]
+
+
+print(memoizeMinCoins(coins=[2,5,10], V=10))
